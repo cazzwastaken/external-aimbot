62e23a0d39384b7c725827d1e348b8c81711494a
cazz <cazzwastaken@gmail.com>
2023-08-25 21:53:42 +0200

    Removed useless references

--- vector.h
+++ vector.h
@@ -5,35 +5,35 @@
 struct Vector3
 {
 	// constructor
-	constexpr Vector3(
+	Vector3(
 		const float x = 0.f,
 		const float y = 0.f,
 		const float z = 0.f) noexcept :
 		x(x), y(y), z(z) { }
 
 	// operator overloads
-	constexpr const Vector3& operator-(const Vector3& other) const noexcept
+	const Vector3 operator-(const Vector3& other) const noexcept
 	{
 		return Vector3{ x - other.x, y - other.y, z - other.z };
 	}
 
-	constexpr const Vector3& operator+(const Vector3& other) const noexcept
+	const Vector3 operator+(const Vector3& other) const noexcept
 	{
 		return Vector3{ x + other.x, y + other.y, z + other.z };
 	}
 
-	constexpr const Vector3& operator/(const float factor) const noexcept
+	const Vector3 operator/(const float factor) const noexcept
 	{
 		return Vector3{ x / factor, y / factor, z / factor };
 	}
 
-	constexpr const Vector3& operator*(const float factor) const noexcept
+	const Vector3 operator*(const float factor) const noexcept
 	{
 		return Vector3{ x * factor, y * factor, z * factor };
 	}
 
 	// utils
-	constexpr const Vector3& ToAngle() const noexcept
+	const Vector3 ToAngle() const noexcept
 	{
 		return Vector3{
 			std::atan2(-z, std::hypot(x, y)) * (180.0f / std::numbers::pi_v<float>),
@@ -41,7 +41,7 @@ struct Vector3
 			0.0f };
 	}
 
-	constexpr const bool IsZero() const noexcept
+	const bool IsZero() const noexcept
 	{
 		return x == 0.f && y == 0.f && z == 0.f;
 	}
